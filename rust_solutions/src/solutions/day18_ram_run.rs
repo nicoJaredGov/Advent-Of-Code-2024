bb6f384e6e845121499c1f233657ed1da89b07ea
Nico Govindsamy <ngovindsamy18@gmail.com>
2025-08-09 13:00:59 +0200

    extracted A star algorithm to own function

--- rust_solutions/src/solutions/day18_ram_run.rs
+++ rust_solutions/src/solutions/day18_ram_run.rs
@@ -8,26 +8,29 @@ pub struct Node {
     future_cost: i32,
 }
 
-pub fn sol(input: &str, board_length: i32, limit: usize) {
-    let obstacles = utils::get_2d_obstacles_set(input, limit);
-    let destination = (board_length - 1, board_length - 1);
-    let start = Node {
-        cell: (0, 0),
-        dist: 0,
-        future_cost: MAX,
-    };
-    let deltas = [(0, 1), (1, 0), (-1, 0), (0, -1)];
-    utils::draw_grid(&obstacles, board_length, board_length);
+// returns true if destination is found else false
+pub fn a_star_search(
+    obstacles: &HashSet<(i32, i32)>,
+    grid_dim: (i32, i32),
+    start_cell: (i32, i32),
+    dest_cell: (i32, i32),
+) -> bool {
+    let (width, height) = grid_dim;
 
     let is_valid_position = |pos: &(i32, i32)| -> bool {
-        let is_out_of_bounds =
-            pos.0 < 0 || pos.0 >= board_length || pos.1 < 0 || pos.1 >= board_length;
+        let is_out_of_bounds = pos.0 < 0 || pos.0 >= width || pos.1 < 0 || pos.1 >= height;
         if is_out_of_bounds || obstacles.contains(&pos) {
             return false;
         }
         true
     };
 
+    let start = Node {
+        cell: start_cell,
+        dist: 0,
+        future_cost: MAX,
+    };
+    let deltas = [(0, 1), (1, 0), (-1, 0), (0, -1)];
     let mut frontier: Vec<Node> = vec![start];
     let mut visited: HashSet<(i32, i32)> = HashSet::new();
 
@@ -35,10 +38,10 @@ pub fn sol(input: &str, board_length: i32, limit: usize) {
         frontier.sort_by(|a, b| (b.dist + b.future_cost).cmp(&(a.dist + a.future_cost)));
         let curr = frontier.pop().unwrap();
 
-        if curr.cell == destination {
+        if curr.cell == dest_cell {
             let path_length = curr.dist;
             println!("{path_length}");
-            return;
+            return true;
         }
 
         visited.insert(curr.cell);
@@ -49,7 +52,7 @@ pub fn sol(input: &str, board_length: i32, limit: usize) {
                 let next_position = Node {
                     cell: next,
                     dist: curr.dist + 1,
-                    future_cost: utils::calculate_manhattan(&next, &destination),
+                    future_cost: utils::calculate_manhattan(&next, &dest_cell),
                 };
                 frontier.push(next_position);
             }
@@ -57,4 +60,14 @@ pub fn sol(input: &str, board_length: i32, limit: usize) {
     }
 
     println!("Destination was not reached.");
+    return false;
+}
+
+pub fn sol(input: &str, board_length: i32, limit: usize) {
+    let obstacles = utils::get_2d_obstacles_set(input, limit);
+    let start = (0, 0);
+    let dest = (board_length - 1, board_length - 1);
+    utils::draw_grid(&obstacles, board_length, board_length);
+
+    a_star_search(&obstacles, (board_length, board_length), start, dest);
 }
